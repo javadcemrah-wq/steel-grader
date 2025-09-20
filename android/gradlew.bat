b8e1c99884530a5bdabaf90be426e53fc192f79a
javadcemrah-wq <javadcemrah@gmail.com>
2025-09-20 17:27:38 +0330

    Add files via upload

---
+++ android/gradlew.bat
@@ -0,0 +1,5 @@
+@echo off
+REM This is a minimal gradlew.bat stub included for completeness.
+REM On CI, Windows builds will use installed Gradle or the official wrapper if present.
+set DIRNAME=%~dp0
+"%DIRNAME%\gradlew" %*
